0be6e3281857e80a2dcb9f203b9c234ea8b019e5
Kiah Reading <30306309+kiahreading@users.noreply.github.com>
2021-03-30 15:21:06 +1000

    correct spelling

--- snippets/blocks/accordion.php
+++
@@ -1,9 +0,0 @@
-<?php $items = $block->accordion()->toStructure();
-foreach ($items as $item): ?>
-	<details <?php e($item->toggle()->bool(), 'open') ?>>
-		<?php if ($item->summary()->isNotEmpty()): ?>
-		<summary><?= $item->summary() ?></summary>
-		<?php endif ?>
-		<?= $item->details()->text() ?>
-	</details>
-<?php endforeach ?>
